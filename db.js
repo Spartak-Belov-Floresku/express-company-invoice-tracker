cfbbe6c699ce854e6b35e77b361b4a962c130ad6
Spartak BF <job.spartak@gmail.com>
2021-08-19 14:47:23 -0400

    express-biztime

---
+++ db.js
@@ -0,0 +1,17 @@
+const { Client } = require("pg");
+
+let DB_URL = {
+    host: "localhost",
+    user: "biztime_user",
+    port: 5432,
+    password: "password",
+    database: ""
+}
+
+DB_URL.database = (process.env.NODE_ENV === "test")? "biztime": "biztime";
+
+
+let db = new Client(DB_URL);
+db.connect();
+
+module.exports = db;
